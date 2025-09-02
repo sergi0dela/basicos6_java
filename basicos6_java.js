e6aaef0437e8d135d142a80d88b985231528d602
d3lac0baa <sercoga2006@gmail.com>
2025-09-03 00:55:13 +0200

    upload

---
+++ basicos6_java.js
@@ -0,0 +1,20 @@
+// Ejercicio 1.1
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }
+  
+  // Ejercicio 1.2
+  for (let i = 0; i < 10; i++) {
+    if (i % 2 === 0) {
+      console.log(i);
+    }
+  }
+  
+  // Ejercicio 1.3
+  for (let i = 1; i <= 10; i++) {
+    if (i === 10) {
+      console.log('¡Dormido!');
+    } else {
+      console.log('Intentando dormir 🐑');
+    }
+  }
